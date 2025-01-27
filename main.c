163d3465c19afa0c752b03786775eeccae088dbe
brianoure <61065227+brianoure@users.noreply.github.com>
2025-01-27 15:26:12 +0430

    Update main.c

--- main.c
+++ main.c
@@ -1,79 +1,161 @@
 int main(){//main
 
+int CLOCKPAUSE=0;
+int CLOCKREAD =0;
 
-//BOARD or SYSTEM transfer
-int PING = 0; //command
-int ACK = 0; 
-int NACK= 0;
-int SON= 0; //command
-int P= 0;
-int UH= 0;
-int AD= 0;
-int GP= 0;
-int SOF= 0; //command
-int SM= 0;//command
-int GM= 0;//command
-int GSC= 0;//command
-int SSC= 0;//command
-int GFP= 0;//command
-int SFP= 0;//command
-int FON= 0;//command
-int TWELVEBUS= 0;
-int FIVEBUS= 0;
-int THREEBUS= 0;
-int FOF= 0;//command
-int GOSTM= 0;//command
-int KEN= 0;//command
-int KDIS= 0;//command
-int GD= 0;//command
-int PD= 0;//command
-int RD= 0;//command
-int WD= 0;//command
-int INITMODE= 0;
-int DETMODE= 0;
-int NORMODE= 0;
-int COMMODE= 0;
-int PLDMODE= 0;
-int IMGMODE= 0;
-int EMMODE= 0;
-int PL5V_EN= 0;
-int ADCS5V_EN= 0;
-int RS12V_EN= 0;
-int XB12V_EN= 0;
-int CCU5V_EN= 0;
-int CCU5V_EN= 0;
-int RS3V3_EN= 0;
-int PL_EN= 0;
-int ADCS_EN= 0;
-int UHF_EN= 0;
-int GPS_EN= 0;
-int ADCS12V_EN= 0;
+int PING      = 0;//command
+int ACK       = 0;//response
+int NACK      = 0;//response
+int SON       = 0;//command
+int PAYLOAD   = 0;//parameter
+int UHF       = 0;//parameter
+int ADCS      = 0;//parameter
+int GPS       = 0;//parameter
+int SOF       = 0;//command
+int SM        = 0;//command
+int GM        = 0;//command
+int GSC       = 0;//command
+int SSC       = 0;//command
+int GFP       = 0;//command
+int SFP       = 0;//command
+int FON       = 0;//command
+int TWELVEBUS = 0;//parameter
+int FIVEBUS   = 0;//parameter
+int THREEBUS  = 0;//parameter
+int FOF       = 0;//command
+int GOSTM     = 0;//command
+int KEN       = 0;//command
+int KDIS      = 0;//command
+int GD        = 0;//command
+int PD        = 0;//command
+int RD        = 0;//command
+int WD        = 0;//command
+int INITIALIZE    = 0;//parameter
+int DETUMBLE      = 0;//parameter
+int NORMAL        = 0;//parameter
+int COMMUNICATION = 0;//parameter
+int PAYLOAD       = 0;//parameter
+int IMAGE         = 0;//parameter
+int EMERGENCY = 0;//pin
+int PL5V_EN   = 0;//pin
+int ADCS5V_EN = 0;//pin
+int RS12V_EN  = 0;//pin
+int XB12V_EN  = 0;//pin
+int CCU5V_EN  = 0;//pin
+int CCU5V_EN  = 0;//pin
+int RS3V3_EN  = 0;//pin
+int PL_EN     = 0;//pin
+int ADCS_EN   = 0;//pin
+int UHF_EN    = 0;//pin
+int GPS_EN    = 0;//pin
+int ADCS12V_EN= 0;//pin
 
-int read_command_input(){return 0;}
+
+int CURRENTMODE       =0;
+int CURRENTSYSTEMCLOCK=0;
+
+int COMMANDARRAY[16];
+
+int read_command_input(){
+//if clock high, wait till clock off
+return 0;
+}//read_command_input
+
+int response(int VALUE1, int VALUE2){
+//if clock off, wait till clock off
+return 0;
+}//response
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 while(1){//while
 int current_command = read_command_input();
-if(current_command==PING){}
-if(current_command==SON){}
-if(current_command==SOF){}
-if(current_command==SM){}
-if(current_command==GM){}
-if(current_command==GSC){}
-if(current_command==SSC){}
-if(current_command==GFP){}
-if(current_command==SFP){}
-if(current_command==FON){}
-if(current_command==FOF){}
-if(current_command==GOSTM){}
-if(current_command==KEN){}
-if(current_command==KDIS){}
-if(current_command==GD){}
-if(current_command==PD){}
-if(current_command==RD){}
-if(current_command==WD){} 
-}//while
+  
+if(current_command==PING  ){response(ACK,0);}
 
-//BOARD or SYSTEM transfer
+//if(current_command==GD  ){response(ACK,0);}
+
+//if(current_command==PD  ){response(ACK,0);}
+
+//if(current_command==RD  ){response(ACK,0);}
+
+//if(current_command==WD  ){response(ACK,0);}
+  
+//SON
+if(current_command==SON_PL5V_EN    ){response(ACK ,0);}
+if(current_command==SON_ADCS5V_EN  ){response(ACK ,0);}
+if(current_command==SON_RS12V_EN   ){response(ACK ,0);}
+if(current_command==SON_XB12V_EN   ){response(ACK ,0);}
+if(current_command==SON_CCU5V_EN   ){response(ACK ,0);}
+if(current_command==SON_RS3V3_EN   ){response(ACK ,0);}
+if(current_command==SON_PL_EN      ){response(ACK ,0);}
+if(current_command==SON_ADCS_EN    ){response(ACK ,0);}
+if(current_command==SON_UHF_EN     ){response(ACK ,0);}
+if(current_command==SON_GPS_EN     ){response(ACK ,0);}
+if(current_command==SON_ADCS12V_EN ){response(ACK ,0);}
+if(current_command==SON_else       ){response(NACK,0);}
+//SON
+
+//SOF  
+if(current_command==SOF_PL5V_EN    ){response(ACK ,0);}
+if(current_command==SOF_ADCS5V_EN  ){response(ACK ,0);}
+if(current_command==SOF_RS12V_EN   ){response(ACK ,0);}
+if(current_command==SOF_XB12V_EN   ){response(ACK ,0);}
+if(current_command==SOF_CCU5V_EN   ){response(ACK ,0);}
+if(current_command==SOF_RS3V3_EN   ){response(ACK ,0);}
+if(current_command==SOF_PL_EN      ){response(ACK ,0);}
+if(current_command==SOF_ADCS_EN    ){response(ACK ,0);}
+if(current_command==SOF_UHF_EN     ){response(ACK ,0);}
+if(current_command==SOF_GPS_EN     ){response(ACK ,0);}
+if(current_command==SOF_ADCS12V_EN ){response(ACK ,0);}
+if(current_command==SOF_else       ){response(NACK,0);}
+//SOF
+
+//SM
+if(current_command==SM_INITIALIZE     ){response(ACK,0);}
+if(current_command==SM_DETUMBLE       ){response(ACK,0);}
+if(current_command==SM_NORMAL         ){response(ACK,0);}
+if(current_command==SM_COMMUNICATION  ){response(ACK,0);}
+if(current_command==SM_PAYLOAD        ){response(ACK,0);}
+if(current_command==SM_IMAGE          ){response(ACK,0);}
+if(current_command==SM_EMERGENCY      ){response(ACK,0);}
+//SM
+
+
+if(current_command==GM   ){response(ACK,CURRENTMODE       );}
+if(current_command==GSC  ){response(ACK,CURRENTSYSTEMCLOCK);}
+if(current_command==SSC_  ){response(ACK,0);}
+
+
+//if(current_command==GFP  ){response(ACK,0);}
+
+  
+//if(current_command==SFP  ){response(ACK,0);}
+
+  
+//if(current_command==FON  ){response(ACK,0);}
+
+  
+//if(current_command==FOF  ){response(ACK,0);}
+
+
+if(current_command==GOSTM){response(ACK,0);}
+if(current_command==KEN  ){response(ACK,0);}
+if(current_command==KDIS ){response(ACK,0);}
+
+}//while
 
   
 return 0;
