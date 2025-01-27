c5244db2eec10d655a04fa2338b10dd8fc6472bf
brianoure <61065227+brianoure@users.noreply.github.com>
2025-01-27 23:54:59 +0430

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
+
 int main(){//main
 
-int CLOCKREAD=0;
+
+int HIGH      =1;
+int LOW       =0;
+int CLOCKREAD =0;
 int CLOCKPAUSE=0;
-int clock(){}
 
 int PING      = 0;//command
 int ACK       = 0;//response
@@ -55,123 +58,194 @@ int ADCS12V_EN= 0;//pin
 int CURRENTMODE       =0;
 int CURRENTSYSTEMCLOCK=0;
 
-int COMMANDARRAY[16];
 
-int read_command_input(){
-//if clock high, wait till clock off
+int COMMAND_RESULT1=0;
+int COMMAND_RESULT2=0;
+
+int COMMAND_ARRAY_INDEX_COUNTER=0;
+
+int RESPONSE_WAIT=10000;
+
+int COMMANDARRAY [16];
+int RESPONSEARRAY[16];
+
+//Initialisation of variables    
+for(int index=0;index++;index<=15){
+   int COMMANDARRAY [index]=0;
+   int RESPONSEARRAY[index]=0;  
+}//for
+//Initialisation of variables
+
+//response_wait
+int response_wait(){
+for(int count=0;count++;count<=RESPONSE_WAIT){}
 return 0;
-}//read_command_input
+}//response_wait
 
-int response(int VALUE1, int VALUE2){
-//if clock off, wait till clock off
+
+//shift all to left, insert new bit at end
+//command_leftShift_insertEnd
+int command_leftShift_insertEnd(){
+    for(int index=0;index++;index<=14){
+    COMMANDARRAY[index] = COMMANDARRAY[index+1];
+    }//for
+    COMMANDARRAY[15]=read_input();
+return 0;  
+}//command_leftShift_insertEnd
+
+   
+//clock
+int clock(){
 return 0;
-}//response
+}//clock
 
 
+//read_input
+int read_input(){
+return 0;
+}//read_input
 
 
+//two_byte_respond
+int two_byte_respond(){
+    for(int index=0;index++;index<=15){
+    RESPONSEARRAY[index] ; // TBC
+    response_wait();
+    }//for
+return 0;
+}//two_byte_respond
+
+       
+//ack_response
+int ack_response(){
+    for(int index=0;index++;index<=7){
+    RESPONSEARRAY[index] = (int) (  ((int)(ACK>>(7-index))) & 1    );
+    }//for
+return 0;
+}//ack_response
 
 
+//nack_response
+int nack_response(){
+    for(int index=0;index++;index<=7){
+    RESPONSEARRAY[index] = (int) (  ((int)(NACK>>(7-index))) & 1    );
+    }//for
+return 0;
+}//nack_response
+   
 
 
+//my_response
+int my_response( int myvalue){
+    for(int index=0;index++;index<=7){
+    RESPONSEARRAY[index] = (int) (  ((int)(myvalue>>(7-index))) & 1    );
+    }//for
+return 0;
+}//my_response
+   
+   
+//capture_command    
+int captured_command(){
+    for(int i=0;i++;i<=7){
+    COMMAND_RESULT1 = COMMAND_RESULT1 + ( COMMANDARRAY[ i ] * ((int)(1<<(7-i))) );
+    }//for
+    for(int i=8;i++;i<=15){
+    COMMAND_RESULT2 = COMMAND_RESULT2 + ( COMMANDARRAY[ i ] * ((int)(1<<(15-i))) );
+    }//for
+return 0;
+}//captured_command
 
+  
+//execute
+int execute(){
+    //PING
+    if( (COMMAND_RESULT1==PING) & (COMMAND_RESULT1==PING)  ){response(ACK,0);}
+    //SON
+    if (COMMAND_RESULT1==SON){
+                             int else_check=1;
+                             if(COMMAND_RESULT2==PL5V_EN    ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==ADCS5V_EN  ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==RS12V_EN   ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==XB12V_EN   ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==RS3V3_EN   ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==PL_EN      ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==ADCS_EN    ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==UHF_EN     ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==GPS_EN     ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==ADCS12V_EN ){else_check=0;ack_response();}//ACK
+                             if(else_check==1               ){nack_response();}//NACK
+    }//son
+    //SOF  
+    if (COMMAND_RESULT1==SOF){
+                             int else_check=1;
+                             if(COMMAND_RESULT2==PL5V_EN    ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==ADCS5V_EN  ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==RS12V_EN   ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==XB12V_EN   ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==RS3V3_EN   ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==PL_EN      ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==ADCS_EN    ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==UHF_EN     ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==GPS_EN     ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==ADCS12V_EN ){else_check=0;ack_response();}//ACK
+                             if(else_check==1               ){nack_response();}//NACK
+    }//son
+    //SOF
+    //SM
+    if (COMMAND_RESULT1==SOF){
+                             int else_check=1;
+                             if(COMMAND_RESULT2==INITIALIZE    ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==DETUMBLE      ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==NORMAL        ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==COMMUNICATION ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==PAYLOAD       ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==IMAGE         ){else_check=0;ack_response();}//ACK
+                             if(COMMAND_RESULT2==EMERGENCY     ){else_check=0;ack_response();}//ACK
+                             if(else_check==1                  ){nack_response();}//NACK
+    }//SM
+    //GM
+    if (COMMAND_RESULT1==GM  ){ack_response();  my_response(CURRENTMODE       );}//ACK
+    //GM
+    //GSC
+    if (COMMAND_RESULT1==GSC ){ack_response();  my_response(CURRENTSYSTEMCLOCK);}//ACK
+    //GSC
+    if (COMMAND_RESULT1==SSC ){ack_response();  /*my_response(0)*/              }//ACK
+    //GSC
+    //GOSTM
+    if(COMMAND_RESULT1==GOSTM){ack_response();  int resp = (int)();my_response(   resp    );             }//ACK
+    //GOSTM
+    //KEN
+    if(COMMAND_RESULT1==KEN  ){ack_response();  /*my_response(0)*/              }//ACK
+    //KEN
+    //KDIS
+    if(COMMAND_RESULT1==KDIS ){ack_response();  /*my_response(0)*/              }//ACK
+    //KDIS
+return 0;
+}//execute
 
-
-
-
-
-
+//MAIN LOOP
 while(1){//while
-
-if( clock()==CLOCKREAD  ){ 
-while( clock()==CLOCKREAD  ){}
-}
-
-if( clock()==CLOCKPAUSE ){
-while( clock()==CLOCKPAUSE ){}
-} 
-
-
-
-
-
-
-int current_command = read_command_input();
-  
-if(current_command==PING  ){response(ACK,0);}
-
-//if(current_command==GD  ){response(ACK,0);}
-
-//if(current_command==PD  ){response(ACK,0);}
-
-//if(current_command==RD  ){response(ACK,0);}
-
-//if(current_command==WD  ){response(ACK,0);}
-  
-//SON
-if(current_command==SON_PL5V_EN    ){response(ACK ,0);}
-if(current_command==SON_ADCS5V_EN  ){response(ACK ,0);}
-if(current_command==SON_RS12V_EN   ){response(ACK ,0);}
-if(current_command==SON_XB12V_EN   ){response(ACK ,0);}
-if(current_command==SON_CCU5V_EN   ){response(ACK ,0);}
-if(current_command==SON_RS3V3_EN   ){response(ACK ,0);}
-if(current_command==SON_PL_EN      ){response(ACK ,0);}
-if(current_command==SON_ADCS_EN    ){response(ACK ,0);}
-if(current_command==SON_UHF_EN     ){response(ACK ,0);}
-if(current_command==SON_GPS_EN     ){response(ACK ,0);}
-if(current_command==SON_ADCS12V_EN ){response(ACK ,0);}
-if(current_command==SON_else       ){response(NACK,0);}
-//SON
-
-//SOF  
-if(current_command==SOF_PL5V_EN    ){response(ACK ,0);}
-if(current_command==SOF_ADCS5V_EN  ){response(ACK ,0);}
-if(current_command==SOF_RS12V_EN   ){response(ACK ,0);}
-if(current_command==SOF_XB12V_EN   ){response(ACK ,0);}
-if(current_command==SOF_CCU5V_EN   ){response(ACK ,0);}
-if(current_command==SOF_RS3V3_EN   ){response(ACK ,0);}
-if(current_command==SOF_PL_EN      ){response(ACK ,0);}
-if(current_command==SOF_ADCS_EN    ){response(ACK ,0);}
-if(current_command==SOF_UHF_EN     ){response(ACK ,0);}
-if(current_command==SOF_GPS_EN     ){response(ACK ,0);}
-if(current_command==SOF_ADCS12V_EN ){response(ACK ,0);}
-if(current_command==SOF_else       ){response(NACK,0);}
-//SOF
-
-//SM
-if(current_command==SM_INITIALIZE     ){response(ACK,0);}
-if(current_command==SM_DETUMBLE       ){response(ACK,0);}
-if(current_command==SM_NORMAL         ){response(ACK,0);}
-if(current_command==SM_COMMUNICATION  ){response(ACK,0);}
-if(current_command==SM_PAYLOAD        ){response(ACK,0);}
-if(current_command==SM_IMAGE          ){response(ACK,0);}
-if(current_command==SM_EMERGENCY      ){response(ACK,0);}
-//SM
-
-
-if(current_command==GM   ){response(ACK,CURRENTMODE       );}
-if(current_command==GSC  ){response(ACK,CURRENTSYSTEMCLOCK);}
-if(current_command==SSC_  ){response(ACK,0);}
-
-
-//if(current_command==GFP  ){response(ACK,0);}
-
-  
-//if(current_command==SFP  ){response(ACK,0);}
-
-  
-//if(current_command==FON  ){response(ACK,0);}
-
-  
-//if(current_command==FOF  ){response(ACK,0);}
-
-
-if(current_command==GOSTM){response(ACK,0);}
-if(current_command==KEN  ){response(ACK,0);}
-if(current_command==KDIS ){response(ACK,0);}
-
-}//while
-
-  
+        int SKIP = 0;
+        //HIGH
+        if (clock() == HIGH) {
+                             //shift all to left, insert new bit at end
+                             command_leftShift_insertEnd();
+                             captured_command();//int
+                             //execute();//int
+                             //two_byte_respond();
+                             while(clock()==HIGH){}
+                             SKIP=1;
+        }//if
+        //HIGH
+        //LOW
+        if( (SKIP==0) & (clock()==LOW) ){
+                                         execute();//int
+                                         two_byte_respond();
+                                         while(clock()==LOW){}
+        }//if
+        //LOW
+}//while  
+//MAIN LOOP
+   
 return 0;
 }//main
