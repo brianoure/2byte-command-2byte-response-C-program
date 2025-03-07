7298284ae1456b8e1cbab56ba4a35551d6d193ae
brianoure <61065227+brianoure@users.noreply.github.com>
2025-03-07 12:23:31 +0430

    Create main.c

---
+++ main.c
@@ -0,0 +1,6 @@
+/*
+*Oops!
+*We've now branched out to several instances of main with an increased number of protocols (rs485,spi,i2c).
+*The "main" files files are located in the home directory/repository.
+*ALUTA CONTINUA!
+*/
